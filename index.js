381b696d5b105cd2a1573861a29f84a4633bc1d5
Sethonyango-13238 <onyangosethoyugi@gmail.com>
2024-07-04 00:21:03 +0300

    mathematical

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+const multiply=num1 * num2;
+console.log(multiply);
+
+const num3=32;
+const num4=7;
+const mod=num3 % num4 ;
+console.log(mod);
+
+const numbersArr=[2,5,20,16,8]
+const max=math.max(...numbersArr);
+console.log(max);
