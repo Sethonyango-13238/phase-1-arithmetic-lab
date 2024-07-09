b473a0f5255f1ac9cd2d7fdd3fc3d6e680b066b3
Sethonyango-13238 <onyangosethoyugi@gmail.com>
2024-07-09 21:08:37 +0300

    arithmetic

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const num4=7;
 const mod=num3 % num4 ;
 console.log(mod);
 
-const numbersArr=[2,5,20,16,8]
-const max=math.max(...numbersArr);
+const numbers=[8,10,12,14,20,16,10];
+const max=Math.max(...numbers);
 console.log(max);
+
+
+const random=Math.floor(Math.random() *10) +1;
+console.log(random);
